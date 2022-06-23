7984f2d490c19d43c03edd17ea9cf42b52e33f10
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-06-23 17:31:38 +0200

    wip: Setup testing for SCIP!

--- tools/scripts/update_exp_files.sh
+++ tools/scripts/update_exp_files.sh
@@ -16,7 +16,7 @@ gems/sorbet/test/hidden-method-finder/update_hidden_methods_exp.sh "$@"
 ./bazel test \
   //gems/sorbet/test/snapshot:update \
   //gems/sorbet-runtime:update_call_validation \
-  //test/cli:update //test/lsp:update -c opt "$@"
+  //test/cli:update //test/lsp:update //test/scip:update -c opt "$@"
 
 tools/scripts/update_testdata_exp.sh
 test/cli/update_cli_exp_files.sh
