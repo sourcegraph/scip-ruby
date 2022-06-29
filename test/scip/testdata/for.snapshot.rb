5d2829b5da0eb10d0e84549bf587d1291ebb7d25
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-06-30 00:38:46 +0200

    feat: Add support for for loops. (#8)
    The variable bound the the loop 'just works', but we need some
    extra fixes to make sure we don't emit needless refs/defs for
    temporaries.

---
+++ test/scip/testdata/for.snapshot.rb
@@ -0,0 +1,25 @@
+ # typed: true
+ 
+ def for_loop()
+#^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO for_loop().
+#^^^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+   y = 0
+#  ^ definition local 1~#1120785331
+   for x in [1, 2, 3]
+#      ^ definition local 2~#1120785331
+     y += x
+#    ^ reference local 1~#1120785331
+#    ^ reference (write) local 1~#1120785331
+#         ^ reference local 2~#1120785331
+     for x in [3, 4, 5]
+#        ^ definition local 3~#1120785331
+       y += x
+#      ^ reference local 1~#1120785331
+#      ^ reference (write) local 1~#1120785331
+#      ^^^^^^ reference local 1~#1120785331
+#           ^ reference local 3~#1120785331
+     end
+   end
+   y
+#  ^ reference local 1~#1120785331
+ end
