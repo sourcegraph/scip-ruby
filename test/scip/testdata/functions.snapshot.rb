e3a59c0e26d9e26ec49f24abfd118f517b192e90
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-11 06:28:32 +0800

    cleanup: Split large test + delete scratchpad test.

---
+++ test/scip/testdata/functions.snapshot.rb
@@ -0,0 +1,19 @@
+ # typed: true
+ 
+ def globalFn1()
+#^^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO globalFn1().
+#^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+   x = 10
+#  ^ definition local 1~#3846536873
+   x
+#  ^ reference local 1~#3846536873
+ end
+ 
+ def globalFn2()
+#^^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO globalFn2().
+   x = globalFn1()
+#  ^ definition local 1~#3796204016
+#  ^^^^^^^^^^^^^^^ reference local 1~#3796204016
+#      ^^^^^^^^^ reference scip-ruby gem TODO TODO globalFn1().
+ end
+ 
