7053d6d8ef5f6eb36a6b2c7e2aeb3de8a3072dc5
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-11 08:24:38 +0800

    cleanup: Split large test + delete scratchpad test. (#11)

---
+++ test/scip/testdata/arrays.snapshot.rb
@@ -0,0 +1,24 @@
+ # typed: true
+ 
+ def arrays(a, i)
+#^^^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO arrays().
+#^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+#           ^ definition local 1~#513334479
+#              ^ definition local 2~#513334479
+   a[0] = 0
+#  ^ reference local 1~#513334479
+   a[1] = a[2]
+#  ^ reference local 1~#513334479
+#         ^ reference local 1~#513334479
+   a[i] = a[i + 1]
+#  ^ reference local 1~#513334479
+#    ^ reference local 2~#513334479
+#         ^ reference local 1~#513334479
+#           ^ reference local 2~#513334479
+   b = a[2..-1]
+#  ^ definition local 3~#513334479
+#      ^ reference local 1~#513334479
+   a << a[-1]
+#  ^ reference local 1~#513334479
+#       ^ reference local 1~#513334479
+ end
