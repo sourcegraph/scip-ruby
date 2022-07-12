c6793ccd67d1692955ab6f5f7683ad1ceefd1966
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-12 08:12:47 +0800

    test:(wip) Add test for class nested inside module.
    The occurrence source range for M::C isn't quite correct yet.

--- test/scip/testdata/classes.rb
+++ test/scip/testdata/classes.rb
@@ -4,7 +4,13 @@ _ = 0
 
 class C1
   def f()
-    _ = C1.new
+    _a = C1.new
+    _b = M2::C2.new
     return
+  end
+end
+
+module M2
+  class C2
   end
 end
--- test/scip/testdata/classes.snapshot.rb
+++ test/scip/testdata/classes.snapshot.rb
@@ -2,16 +2,29 @@
  
  _ = 0
 #^ definition local 1~#119448696
-#^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+#^^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
  
  class C1
-#^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+#^^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
 #      ^^ definition scip-ruby gem TODO TODO C1#
    def f()
 #  ^^^^^^^ definition scip-ruby gem TODO TODO f().
-     _ = C1.new
-#    ^ definition local 2~#3809224601
-#        ^^ reference scip-ruby gem TODO TODO C1#
+     _a = C1.new
+#    ^^ definition local 2~#3809224601
+#         ^^ reference scip-ruby gem TODO TODO C1#
+     _b = M2::C2.new
+#    ^^ definition local 5~#3809224601
+#         ^^ reference scip-ruby gem TODO TODO M2#
+#         ^^^^^^ reference scip-ruby gem TODO TODO C2#
      return
+   end
+ end
+ 
+ module M2
+#^^^^^^^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+#       ^^ definition scip-ruby gem TODO TODO M2#
+   class C2
+#  ^^^^^^^^^^^^ definition scip-ruby gem TODO TODO <static-init>().
+#        ^^ definition scip-ruby gem TODO TODO C2#
    end
  end
