95af9aad615bc7eb8553856762aea41223c13816
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-12 08:54:01 +0800

    test: Add test for locally-defined classes.

--- test/scip/testdata/classes.rb
+++ test/scip/testdata/classes.rb
@@ -17,3 +17,14 @@ end
 
 class M3::C3
 end
+
+def local_class()
+  localClass = Class.new
+  # Technically, this is not supported by Sorbet (https://srb.help/3001),
+  # but make sure we don't crash or do something weird.
+  def localClass.myMethod()
+    ":)"
+  end
+  _c = localClass.new
+  _m = localClass.myMethod
+end
