ec69f6b550a3cea3d165cf7cf73f690900acbe41
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-08-09 21:54:07 +0800

    ci: Clean up CI workflow with finer caching.

---
+++ .github/workflows/setup-bazel.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CACHE_ROOT="$HOME/bazelcache"
+mkdir -p "$CACHE_ROOT/output-base" "$CACHE_ROOT/build" "$CACHE_ROOT/repos"
+{
+  echo 'common --curses=no --color=yes'
+  echo "startup --output_base=$CACHE_ROOT/output-base"
+  echo "build  --disk_cache=$CACHE_ROOT/build --repository_cache=$CACHE_ROOT/repos"
+  echo "test   --disk_cache=$CACHE_ROOT/build --repository_cache=$CACHE_ROOT/repos --test_output=errors"
+} > .bazelrc.local
+
+# Install Bazel and print version
+./bazel version
