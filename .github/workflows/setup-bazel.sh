d737e49cc0a45373d1b6b80d182c50b7f243127b
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-08-12 15:00:00 +0800

    build: Bazel-ify builds of Gems.
    - Gem building no longer modifies the source tree.
    - It enables using the gems for repo tests.

--- .github/workflows/setup-bazel.sh
+++ .github/workflows/setup-bazel.sh
@@ -8,6 +8,9 @@ mkdir -p "$CACHE_ROOT/output-base" "$CACHE_ROOT/build" "$CACHE_ROOT/repos"
   echo 'common --curses=no --color=yes'
   echo "startup --output_base=$CACHE_ROOT/output-base"
   echo "build  --disk_cache=$CACHE_ROOT/build --repository_cache=$CACHE_ROOT/repos"
+  if [ -n "${VERSION:-}" ]; then
+    echo "build --//gems/scip-ruby:version=$VERSION"
+  fi
   echo "test   --disk_cache=$CACHE_ROOT/build --repository_cache=$CACHE_ROOT/repos --test_output=errors"
 } > .bazelrc.local
 
