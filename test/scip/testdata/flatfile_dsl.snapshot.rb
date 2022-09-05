768fd6ee7be51ba5c9f59619de1d2821e1779807
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-09-05 20:48:45 +0800

    fix: Fix source locations for Flatfile DSL.

---
+++ test/scip/testdata/flatfile_dsl.snapshot.rb
@@ -0,0 +1,88 @@
+ # typed: true
+ 
+ class Record
+#      ^^^^^^ definition [..] Record#
+   def self.flatfile; end
+#           ^^^^^^^^ definition [..] `<Class:Record>`#flatfile().
+   def self.from(*_); end
+#           ^^^^ definition [..] `<Class:Record>`#from().
+   def self.pattern(*_); end
+#           ^^^^^^^ definition [..] `<Class:Record>`#pattern().
+   def self.field(*_); end
+#           ^^^^^ definition [..] `<Class:Record>`#field().
+ end
+ 
+ class Flatfile < Record
+#      ^^^^^^^^ definition [..] Flatfile#
+#                 ^^^^^^ definition [..] Record#
+   flatfile do
+     from   1..2, :foo
+#    ^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#    ^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#params().
+#    ^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#                 ^^^^ definition [..] Flatfile#`foo=`().
+#                 ^^^^ definition [..] Flatfile#foo().
+     pattern(/A-Za-z/, :bar)
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#params().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^^^^^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#            ^^^^^^^^ reference [..] Regexp#
+#                      ^^^^ definition [..] Flatfile#`bar=`().
+#                      ^^^^ definition [..] Flatfile#bar().
+     field :baz
+#    ^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^ reference [..] `<Class:T>`#untyped().
+#    ^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#params().
+#    ^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#    ^^^^^^^^^^ reference [..] T#
+#    ^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^ reference [..] T#Private#Methods#DeclBuilder#returns().
+#    ^^^^^^^^^^ reference [..] Sorbet#Private#Static#
+#    ^^^^^^^^^^ reference [..] Sorbet#Private#`<Class:Static>`#sig().
+#    ^^^^^^^^^^ reference [..] T#
+#          ^^^^ definition [..] Flatfile#`baz=`().
+#          ^^^^ definition [..] Flatfile#baz().
+   end
+ end
+ 
+ t = Flatfile.new
+#^ definition local 1~#119448696
+#    ^^^^^^^^ reference [..] Flatfile#
+ t.foo = t.foo + 1
+#^ reference local 1~#119448696
+#  ^^^^^ reference [..] Flatfile#`foo=`().
+#        ^ reference local 1~#119448696
+#          ^^^ reference [..] Flatfile#foo().
+ t.bar = t.bar + 1
+#^ reference local 1~#119448696
+#  ^^^^^ reference [..] Flatfile#`bar=`().
+#        ^ reference local 1~#119448696
+#          ^^^ reference [..] Flatfile#bar().
+ t.baz = t.baz + 1
+#^ reference local 1~#119448696
+#  ^^^^^ reference [..] Flatfile#`baz=`().
+#        ^ reference local 1~#119448696
+#          ^^^ reference [..] Flatfile#baz().
