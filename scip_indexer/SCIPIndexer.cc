1068b29f56d6108722a8091c03389114dc89a6e0
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-11-11 13:30:58 +0800

    docs: Add CHANGELOG entry for v0.3.1 (#161)

--- scip_indexer/SCIPIndexer.cc
+++ scip_indexer/SCIPIndexer.cc
@@ -61,7 +61,7 @@ static uint32_t fnv1a_32(const string &s) {
     return h;
 }
 
-const char scip_ruby_version[] = "0.3.0";
+const char scip_ruby_version[] = "0.3.1";
 
 // Last updated: https://github.com/sourcegraph/scip-ruby/pull/145
 const char scip_ruby_sync_upstream_sorbet_sha[] = "bc92a0f55897bda974df401a3a1b4d2191e0877b";
