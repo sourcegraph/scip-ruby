09a72c49ce08c93d22967662838015c3fef72d5f
Varun Gandhi <varun.gandhi@sourcegraph.com>
2024-07-16 21:27:01 +0800

    fix: Fix code navigation for enum values

--- test/scip/testdata/enum.rb
+++ test/scip/testdata/enum.rb
@@ -1,10 +1,25 @@
-# typed: struct
+# typed: strict
 
 class X < T::Enum
   enums do
     A = new("A")
     B = new
+    C = B
   end
 
   All = T.let([A, B], T::Array[X])
+end
+
+# Adding more cases like this is not supported (c.f. isTEnum),
+# but let's at least add a test.
+class Y < X
+  enums do
+    D = new
+    E = B
+  end
+end
+
+def use_abc
+  x = X::A
+  return
 end
