bfbaa15413ff3a0bb24be1b594e2e4953f8a08df
Varun Gandhi <varun.gandhi@sourcegraph.com>
2025-01-13 16:57:28 +0800

    fix: Correctly emit references for Command#call (#232)
    Fixes GRAPH-895.
    
    There are two ways to fix this issue, by emitting a definition
    for the synthetic class method generated by Sorbet in rewriter/Command.cc,
    as well as emitting a reference for that; OR merely changing
    the reference emitting logic to also have a special case
    (similar to rewriter/Command.cc). I've picked the second option.
    
    As of now, I don't understand which part of the Sorbet logic
    is responsible for making sure that the synthetic method does
    not show up when using findMethodTransitive which we're
    using for name lookup. So it makes sense to make the simpler
    change here.

---
+++ test/scip/testdata/call.snapshot.rb
@@ -0,0 +1,109 @@
+ # typed: true
+ 
+ # NOTE: The methods have bodies to make sure that we're not
+ # accidentally skipping emitting references for the method body
+ # due to changes in rewriter/Command.cc
+ 
+ class Opus::Command
+#      ^^^^ reference [..] Opus#
+#            ^^^^^^^ definition [..] Opus#Command#
+ end
+ 
+ # NOTE: This is nested inside Opus as a convention,
+ # but the key thing is the subclassing relationship.
+ class Opus::MyThing::Command::GetThing < Opus::Command
+#      ^^^^ reference [..] Opus#
+#            ^^^^^^^ reference [..] Opus#MyThing#
+#                     ^^^^^^^ reference [..] Opus#MyThing#Command#
+#                              ^^^^^^^^ definition [..] Opus#MyThing#Command#GetThing#
+#                                         ^^^^ reference [..] Opus#
+#                                               ^^^^^^^ reference [..] Opus#Command#
+   def call()
+#      ^^^^ definition [..] Opus#MyThing#Command#GetThing#call().
+     x = 1
+#    ^ definition local 1~#3018949801
+     y = x
+#    ^ definition local 2~#3018949801
+#    ^^^^^ reference local 2~#3018949801
+#        ^ reference local 1~#3018949801
+   end
+ end
+ 
+ # Forgot < Opus::Command relationship here
+ class Opus::MyThing::BadCommand::GetThing
+#      ^^^^ reference [..] Opus#
+#            ^^^^^^^ reference [..] Opus#MyThing#
+#                     ^^^^^^^^^^ reference [..] Opus#MyThing#BadCommand#
+#                                 ^^^^^^^^ definition [..] Opus#MyThing#BadCommand#GetThing#
+   def call()
+#      ^^^^ definition [..] Opus#MyThing#BadCommand#GetThing#call().
+     x = 1
+#    ^ definition local 1~#3018949801
+     y = x
+#    ^ definition local 2~#3018949801
+#    ^^^^^ reference local 2~#3018949801
+#        ^ reference local 1~#3018949801
+   end
+ end
+ 
+ class NotOpus::Command1::GetThing
+#      ^^^^^^^ reference [..] NotOpus#
+#               ^^^^^^^^ reference [..] NotOpus#Command1#
+#                         ^^^^^^^^ definition [..] NotOpus#Command1#GetThing#
+   # Class method
+   def self.call()
+#           ^^^^ definition [..] NotOpus#Command1#`<Class:GetThing>`#call().
+     x = 1
+#    ^ definition local 1~#3018949801
+     y = x
+#    ^ definition local 2~#3018949801
+#    ^^^^^ reference local 2~#3018949801
+#        ^ reference local 1~#3018949801
+   end
+ end
+ 
+ class NotOpus::Command2::GetThing
+#      ^^^^^^^ reference [..] NotOpus#
+#               ^^^^^^^^ reference [..] NotOpus#Command2#
+#                         ^^^^^^^^ definition [..] NotOpus#Command2#GetThing#
+   # Instance method
+   def call()
+#      ^^^^ definition [..] NotOpus#Command2#GetThing#call().
+     x = 1
+#    ^ definition local 1~#3018949801
+     y = x
+#    ^ definition local 2~#3018949801
+#    ^^^^^ reference local 2~#3018949801
+#        ^ reference local 1~#3018949801
+   end
+ end
+ 
+ def make_call()
+#    ^^^^^^^^^ definition [..] Object#make_call().
+   # Should navigate to instance method
+   Opus::MyThing::Command::GetThing.call()
+#  ^^^^ reference [..] Opus#
+#        ^^^^^^^ reference [..] Opus#MyThing#
+#                 ^^^^^^^ reference [..] Opus#MyThing#Command#
+#                          ^^^^^^^^ reference [..] Opus#MyThing#Command#GetThing#
+#                                   ^^^^ reference [..] Opus#MyThing#Command#GetThing#call().
+   # Actually wrong, because < Opus::Command was missed
+   Opus::MyThing::BadCommand::GetThing.call()
+#  ^^^^ reference [..] Opus#
+#        ^^^^^^^ reference [..] Opus#MyThing#
+#                 ^^^^^^^^^^ reference [..] Opus#MyThing#BadCommand#
+#                             ^^^^^^^^ reference [..] Opus#MyThing#BadCommand#GetThing#
+   # Not expected to work since type is not in Opus namespace
+   NotOpus::Command1::GetThing.call()
+#  ^^^^^^^ reference [..] NotOpus#
+#           ^^^^^^^^ reference [..] NotOpus#Command1#
+#                     ^^^^^^^^ reference [..] NotOpus#Command1#GetThing#
+#                              ^^^^ reference [..] NotOpus#Command1#`<Class:GetThing>`#call().
+   # Should navigate to instance method
+   NotOpus::Command2::GetThing.new().call()
+#  ^^^^^^^ reference [..] NotOpus#
+#           ^^^^^^^^ reference [..] NotOpus#Command2#
+#                     ^^^^^^^^ reference [..] NotOpus#Command2#GetThing#
+#                              ^^^ reference [..] Class#new().
+#                                    ^^^^ reference [..] NotOpus#Command2#GetThing#call().
+ end
