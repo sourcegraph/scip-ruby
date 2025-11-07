319524058d87cfe5992cfb9eec12ec70fc91213d
Varun Gandhi <varun.gandhi@sourcegraph.com>
2025-11-07 21:14:06 +0800

    fix: Stale check in publish script

--- tools/scripts/publish-scip-ruby.sh
+++ tools/scripts/publish-scip-ruby.sh
@@ -21,11 +21,6 @@ if ! grep -q "const char scip_ruby_version\[\] = \"$NEW_VERSION\"" scip_indexer/
   exit 1
 fi
 
-if ! grep -q "download/scip-ruby-v$NEW_VERSION" Dockerfile.autoindex; then
-  echo "error: scip-ruby version in Dockerfile is not the latest release version."
-  exit 1
-fi
-
 if ! git diff --quiet; then
   echo "error: Found unstaged changes; aborting."
   exit 1
